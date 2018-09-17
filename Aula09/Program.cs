983e60b6f3daed105351f57981801a12c7e90fbe
Renato Matos <renato.matos79@gmail.com>
2018-09-17 01:20:37 -0300

    novos projetos

---
+++ Aula09/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09
+{
+    class Program
+    {
+        const int VALORES = 3;
+        const int MAX = 10;
+        static void Main(string[] args)
+        {
+            int[] valores = new int[VALORES];
+            int numero = 0;
+            bool achou = false;
+            var randon = new Random(0);
+            Console.WriteLine("Informe um numero:");
+            numero = int.Parse(Console.ReadLine());
+            for (int contador = 0; contador < VALORES; contador++)
+            {
+                valores[contador] = randon.Next(1, MAX);
+                if (valores[contador] == numero)
+                {
+                    achou = true;
+                    break;
+                }
+            }
+            if (achou)
+            {
+                Console.WriteLine($"O número {numero} existe na lista!");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} Não existe na lista!");
+                Console.WriteLine("Elementos da lista!");
+                for (int contador = 0; contador < VALORES; contador++)
+                {
+                    Console.WriteLine(valores[contador].ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    class Program
+    {
+        const string FORCA = "COOPJOVEM";
+        const int MAX_ERROS = 3;
+        static void Main(string[] args)
+        {
+            int erros = 0;
+            string palavra = "_";
+            while (palavra.Length < FORCA.Length)
+            {
+                palavra += "_";
+            }
+            while ((erros < MAX_ERROS) && palavra.Contains("_"))
+            {
+                Console.Clear();
+                Console.WriteLine("JOGO DA FORCA!");
+                Console.WriteLine($"A palavra contém apenas {FORCA.Length} caracteres!");
+                Console.WriteLine($"Informe uma letra!");
+                var letra = Console.ReadLine();
+                if (FORCA.Contains(letra) == true)
+                {
+                    for (var contador = 0; contador < FORCA.Length; contador++)
+                    {
+                        if (FORCA[contador] == letra[0])
+                        {
+                            palavra = palavra.Remove(contador, 1).Insert(contador, letra);
+                        }
+                    }
+                }
+                else
+                {
+                    erros++;
+                }
+                Console.WriteLine("");
+                Console.WriteLine($"Palavra: {palavra}");
+                Console.WriteLine($"ERROS: {erros}");
+                Console.ReadLine();
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fim!");
+            Console.ReadLine();
+
+        }
+    }
+}
